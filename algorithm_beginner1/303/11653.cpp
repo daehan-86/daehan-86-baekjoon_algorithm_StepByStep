51a05ad3efb4f7892dddfd20859a333f9792662b
daehan-86 <j95222177@gmail.com>
2022-12-04 08:02:52 +0000

    22-12-04#2

---
+++ algorithm_beginner1/303/11653.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(void){
+	int n;
+	cin>>n;
+	for(int i=2;i*i<=n;i++){
+		while(n%i==0){
+			printf("%d\n",i);
+			n/=i;
+		}
+	}
+	if(n!=1) printf("%d",n);
+	return 0;
+}

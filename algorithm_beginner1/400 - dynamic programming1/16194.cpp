f3449768013e0df53f61bbc25196ba24979653dd
daehan-86 <j95222177@gmail.com>
2022-12-05 10:56:28 +0000

    22-12-05#3

---
+++ algorithm_beginner1/400 - dynamic programming1/16194.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int dp[1001];
+int main(void){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		scanf("%d",&dp[i]);
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			if(i+j<=n){
+				dp[i+j]=min(dp[i+j],dp[i]+dp[j]);
+			}
+		}
+	}
+	cout<<dp[n];
+	return 0;
+}

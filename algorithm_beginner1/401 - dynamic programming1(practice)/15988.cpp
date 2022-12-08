9b621f6814a920c0e6a3b416944b3cacd1990e7d
daehan-86 <j95222177@gmail.com>
2022-12-08 13:52:18 +0000

    22-12-08#4

--- algorithm_beginner1/401 - dynamic programming1(practice)/15988.cpp
+++ algorithm_beginner1/401 - dynamic programming1(practice)/15988.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
+int dp[1000001]={0,1,1,1};
 int main(void){
-	
+	for(int i=1;i<1000001;i++){
+		for(int j=1;j<4;j++){
+			dp[i+j]+=dp[i];
+			dp[i+j]%=1000000009;
+		}
+	}
+	int t,n;
+	cin>>t;
+	while(t--){
+		scanf("%d",&n);
+		printf("%d\n",dp[n]);
+	}
 	return 0;
 }

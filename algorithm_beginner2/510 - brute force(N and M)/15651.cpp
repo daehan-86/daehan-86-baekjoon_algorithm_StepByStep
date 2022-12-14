ab01c7739ce956eac60bdb5672d57f86e9b86d8c
daehan-86 <j95222177@gmail.com>
2022-12-14 13:17:14 +0000

    22-12-14#3

---
+++ algorithm_beginner2/510 - brute force(N and M)/15651.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int n,m;
+void br(int *a,int d){
+	if(d==m){
+		for(int i=0;i<m;i++) printf("%d ",a[i]);
+		printf("\n");
+		return;
+	}
+	for(int i=1;i<=n;i++){
+		a[d]=i;
+		br(a,d+1);
+	}
+}
+int main(void){
+	cin>>n>>m;
+	int a[10];
+	br(a,0);
+	return 0;
+}

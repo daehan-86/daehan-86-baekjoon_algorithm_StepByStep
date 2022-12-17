9243204f61f83a36c8dfa8edd4421a6dbedf104d
daehan-86 <j95222177@gmail.com>
2022-12-17 13:32:40 +0000

    22-12-17#2

---
+++ algorithm_beginner2/510 - brute force(N and M)/15664.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int n,m,v[10],visit[10001],k;
+void br(int *a,int d,int p){
+	if(d==m){
+		for(int i=0;i<m;i++) printf("%d ",a[i]);
+		printf("\n");
+		return;
+	}
+	for(int i=p;i<k;i++){
+		if(visit[v[i]]){
+			a[d]=v[i];
+			visit[v[i]]--;
+			br(a,d+1,i);
+			visit[v[i]]++;
+		}
+	}
+}
+int main(void){
+	cin>>n>>m;
+	int a[10],t;
+	for(int i=0;i<n;i++){
+		scanf("%d",&t);
+		if(!visit[t]){
+			v[k]=t;
+			k++;
+		}
+		visit[t]++;
+	}
+	sort(v,v+k);
+	br(a,0,0);
+	return 0;
+}

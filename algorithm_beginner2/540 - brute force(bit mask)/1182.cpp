f7931d346dca170d3e5e9e6c7c6cb3f20e4c3d33
daehan-86 <j95222177@gmail.com>
2022-12-24 04:08:54 +0000

    22-12-24#3

---
+++ algorithm_beginner2/540 - brute force(bit mask)/1182.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int n,k,v[21],res;
+void br(int t,int p,int s){
+	if(n-1==p){
+		printf("%d %d\n",t,s);
+		if(t>0&&s==k) res++;
+		return;
+	}
+	for(int i=p+1;i<n;i++){
+		br(t+1,i,s+v[i]);
+	}
+	br(t,n-1,s);
+}
+int main(void){
+	cin>>n>>k;
+	for(int i=0;i<n;i++) scanf("%d",v+i);
+	int a[22];
+	br(0,-1,0);
+	cout<<res;
+	return 0;
+}

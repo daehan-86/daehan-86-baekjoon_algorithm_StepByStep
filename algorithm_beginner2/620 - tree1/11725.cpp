b2c508b0de716ef5958c743c55d3c2ab555f1f4f
daehan-86 <j95222177@gmail.com>
2023-01-26 13:34:58 +0000

    23-01-26#3

--- algorithm_beginner2/620 - tree1/11725.cpp
+++ algorithm_beginner2/620 - tree1/11725.cpp
@@ -1,23 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 vector<int>v[100001];
 bool visit[100001];
 int res[100001];
-typedef struct node{
-	vector<node*>ch;
-	node(int x){
-		for(int i=0;i<(int)v[x].size();i++){
-			int &t=v[x][i];
-			if(!visit[t]){
-				visit[t]=1;
-				res[t]=x;
-				node *k=new node(t);
-				ch.push_back(k);
-			}
-		}
-	}
-}node;
+queue<int>q;
 int main(void){
 	int n,a,b;
 	cin>>n;
@@ -26,7 +14,20 @@ int main(void){
 		v[a].push_back(b);
 		v[b].push_back(a);
 	}
-	node *t= new node(1);
+	q.push(1);
+	visit[1]=1;
+	while(!q.empty()){
+		int x=q.front();
+		q.pop();
+		for(int i=0;i<(int)v[x].size();i++){
+			int &t=v[x][i];
+			if(!visit[t]){
+				visit[t]=1;
+				res[t]=x;
+				q.push(t);
+			}
+		}
+	}
 	for(int i=2;i<=n;i++) printf("%d\n",res[i]);
 	return 0;
 }

28867fb9a82831d41d9a59390781c94934f437b5
daehan-86 <j95222177@gmail.com>
2023-06-04 06:38:09 +0000

    23-06-04#11

---
+++ baekjoon_StepByStep/3 FOR/11021.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+	int n,a,b;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		scanf("%d %d",&a,&b);
+		printf("Case #%d: %d\n",i,a+b);
+	}
+	return 0;
+}

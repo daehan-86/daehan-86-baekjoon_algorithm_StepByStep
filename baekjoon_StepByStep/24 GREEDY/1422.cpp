9adf10dec1be4e11b1bc3af35e38a979fc809b43
daehan-86 <j95222177@gmail.com>
2023-06-28 13:42:14 +0000

    23-06-28#1

--- baekjoon_StepByStep/24 GREEDY/1422.cpp
+++ baekjoon_StepByStep/24 GREEDY/1422.cpp
@@ -3,14 +3,26 @@
 #include<string>
 #include<vector>
 using namespace std;
-int n,k;
-vector<int>v;
+int n,k,t,m;
+vector<string>v;
+string s;
+bool cmp(string a,string b){
+	string s1=a+b,s2=b+a;
+	for(int i=0;i<s1.length();i++)
+		if(s1[i]!=s2[i]) return s1[i]>s2[i];
+	return 0;
+}
 int main(void){
 	cin.tie(0);
 	ios::sync_with_stdio(0);
 	cin>>n>>k;
 	for(int i=0;i<n;i++){
-		
+		cin>>t;
+		m=max(m,t);
+		v.push_back(to_string(t));
 	}
+	for(int i=0;i<k-n;i++) v.push_back(to_string(m));
+	sort(v.begin(),v.end(),cmp);
+	for(int i=0;i<v.size();i++) cout<<v[i];
 	return 0;
 }

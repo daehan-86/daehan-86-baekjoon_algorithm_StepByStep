3fb6cc05a1632786da907afe17ff7371d0314da5
daehan-86 <j95222177@gmail.com>
2023-08-19 04:55:46 +0000

    23-08-19#1

--- baekjoon_StepByStep/32 TWO POINTER/16153.cpp
+++ baekjoon_StepByStep/32 TWO POINTER/16153.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-vector<int>v1,v2;
+vector<int>v1,v2,v3;
 int n,a,b,x,y,t,res;
 int main(void){
 	cin.tie(0);
@@ -13,27 +13,27 @@ int main(void){
 		if(t>=15)x|=(1<<t);
 		else y|=(1<<t);
 	}
-	if(b>=(1<<15)){
-		for(int i=0;i<min((b>>15)+1,1<<15);i++){
-			t=(i<<15);
-			if((t&x)==x)v1.push_back(t%a);
-		}
-		for(int i=0;i<(1<<15);i++){
-			if((i&y)==y)v2.push_back(i%a);
-		}
+	if(!x&&!y) res--;
+	for(int i=0;i<(b>>15);i++){
+		t=(i<<15);
+		if((t&x)==x)v1.push_back(t%a);
 	}
-	else{
-		v1.push_back(0);
-		for(int i=0;i<min(b+1,1<<15);i++){
-			if((i&y)==y)v2.push_back(i%a);
-		}
+	for(int i=0;i<(1<<15);i++){
+		if((i&y)==y)v2.push_back(i%a);
 	}
-	//cout<<v1.size()<<" "<<v2.size()<<"\n";
+	for(int i=0;i<=(b&((1<<15)-1));i++){
+		if((i&y)==y)v3.push_back(i%a);
+	}
 	sort(v2.begin(),v2.end());
-	//cout<<v1[0]<<v2[0]<<" ";
+	sort(v3.begin(),v3.end());
 	for(int o:v1){
 		t=(a-o)%a;
 		res+=upper_bound(v2.begin(),v2.end(),t)-lower_bound(v2.begin(),v2.end(),t);
+	}
+	t=((b>>15)<<15);
+	if((t&x)==x){
+		t=(a-(t%a))%a;
+		res+=upper_bound(v3.begin(),v3.end(),t)-lower_bound(v3.begin(),v3.end(),t);
 	}
 	cout<<res;
 	return 0;
